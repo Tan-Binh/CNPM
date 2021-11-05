ea435417681e612d325f688f2606f2d0908fd8c0
Tan-Binh <phantanbinh0505@gmail.com>
2021-11-05 15:13:53 +0700

    Sửa width thành % và cập nhật mục khóa học

--- js/trangChu.js
+++ js/trangChu.js
@@ -5,7 +5,7 @@ $('.list').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
-    draggable: true,
+    draggable: false,
     prevArrow:`<button type='button' class='slick-prev slick-arrow'><i class='fa fa-angle-left' aria-hidden='true'></i></button>`,
     nextArrow:`<button type='button' class='slick-next slick-arrow'><i class='fa fa-angle-right' aria-hidden='true'></i></button>`
 });
