695c90a8f81b599c1da0393a91b806d7cebc1fd2
Tan-Binh <phantanbinh0505@gmail.com>
2021-12-15 15:45:52 +0700

    Chỉnh lại trang khóa học

---
+++ js/khoaHoc.js
@@ -0,0 +1,9 @@
+function Redirect() {
+    window.location="../web/course-2.html";
+ }
+
+const btnMore = document.querySelectorAll(".btn-more");
+for (let index = 0; index < btnMore.length; index++) {
+    const element = btnMore[index];
+    element.addEventListener('click', Redirect);
+}
