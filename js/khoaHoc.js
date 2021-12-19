0dee2e8fad6bd2e0608c77511eff7488fde39e70
Tan-Binh <phantanbinh0505@gmail.com>
2021-12-19 10:44:30 +0700

    bbb

--- js/khoaHoc.js
+++ js/khoaHoc.js
@@ -1,9 +1,18 @@
 function Redirect() {
-    window.location="../web/course-2.html";
- }
+    window.location = "../web/course-2.html";
+}
+function RedirectIndex() {
+    window.location = "./web/course-2.html";
+}
 
 const btnMore = document.querySelectorAll(".btn-more");
 for (let index = 0; index < btnMore.length; index++) {
     const element = btnMore[index];
     element.addEventListener('click', Redirect);
+}
+
+const btnMoreIndex = document.querySelectorAll(".btn-more-index");
+for (let index = 0; index < btnMoreIndex.length; index++) {
+    const element = btnMoreIndex[index];
+    element.addEventListener('click', RedirectIndex);
 }
